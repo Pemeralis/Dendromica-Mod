1f5595e4064dbf7040f70a639d35e82824100303
Pem <pemeralisaiura@gmail.com>
2020-08-06 20:28:54 -0500

    Initial commit

--- src/main/java/dendromica/DendromicaItems.java
+++ src/main/java/dendromica/DendromicaItems.java
@@ -20,12 +20,16 @@ public class DendromicaItems{
     public static ItemGroup DENDROMICA_GROUP;
 
     static {
-        EURENITE_SWORD = new SwordItem(DendromicaMaterials.EURENITE, 3, -2.6F, new Item.Settings().group(ItemGroup.COMBAT).group(DENDROMICA_GROUP));
-        EURENITE_SHOVEL = new ShovelItem(DendromicaMaterials.EURENITE, 1.5F, -3.0F, new Item.Settings().group(ItemGroup.TOOLS));
-        EURENITE_PICKAXE = new PickaxeItem(DendromicaMaterials.EURENITE, 2, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
-        EURENITE_HOE = new HoeItem(DendromicaMaterials.EURENITE, -20, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
-        EURENITE_AXE = new AxeItem(DendromicaMaterials.EURENITE, 5, -3.3F, new Item.Settings().group(ItemGroup.TOOLS)) {};
-        EURENITE = new Item(new Item.Settings().group(ItemGroup.MISC));
+        DENDROMICA_GROUP = FabricItemGroupBuilder.create(
+                new Identifier("tutorial", "other"))
+                .icon(() -> new ItemStack(EURENITE))
+                .build();
+        EURENITE_SWORD = new SwordItem(DendromicaMaterials.EURENITE, 3, -2.6F, new Item.Settings().group(DENDROMICA_GROUP));
+        EURENITE_SHOVEL = new ShovelItem(DendromicaMaterials.EURENITE, 1.5F, -3.0F, new Item.Settings().group(DENDROMICA_GROUP));
+        EURENITE_PICKAXE = new PickaxeItem(DendromicaMaterials.EURENITE, 2, -2.0F, new Item.Settings().group(DENDROMICA_GROUP)) {};
+        EURENITE_HOE = new HoeItem(DendromicaMaterials.EURENITE, -20, -2.0F, new Item.Settings().group(DENDROMICA_GROUP)) {};
+        EURENITE_AXE = new AxeItem(DendromicaMaterials.EURENITE, 5, -3.3F, new Item.Settings().group(DENDROMICA_GROUP)) {};
+        EURENITE = new Item(new Item.Settings().group(DENDROMICA_GROUP));
         EURENITE_ORE = new Block(FabricBlockSettings.of(Material.METAL)
                 .hardness(5.0f)
                 .breakByTool(FabricToolTags.PICKAXES, 3)
@@ -34,9 +38,5 @@ public class DendromicaItems{
                 .hardness(8.0f)
                 .breakByTool(FabricToolTags.PICKAXES, 3)
                 .requiresTool());
-        DENDROMICA_GROUP = FabricItemGroupBuilder.create(
-                new Identifier("tutorial", "other"))
-                .icon(() -> new ItemStack(EURENITE))
-                .build();
     }
 }
