3ef73e365479bdea7bf5b3aecd08ead98f458f62
BlueVsRed12 <dgr8stgamer141@gmail.com>
2020-08-06 20:13:20 -0500

    made a staticy block c:

--- src/main/java/dendromica/DendromicaItems.java
+++ src/main/java/dendromica/DendromicaItems.java
@@ -9,22 +9,32 @@ import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 
 public class DendromicaItems{
+    public static ToolItem EURENITE_SWORD;
+    public static ToolItem EURENITE_SHOVEL;
+    public static ToolItem EURENITE_PICKAXE;
+    public static ToolItem EURENITE_HOE;
+    public static ToolItem EURENITE_AXE;
+    public static Item EURENITE;
+    public static Block EURENITE_ORE;
+    public static Block EURENITE_BLOCK;
+    public static ItemGroup DENDROMICA_GROUP;
+
     static {
-        ToolItem EURENITE_SWORD = new SwordItem(DendromicaMaterials.EURENITE, 3, -2.6F, new Item.Settings().group(ItemGroup.COMBAT).group(DENDROMICA_GROUP));
-        ToolItem EURENITE_SHOVEL = new ShovelItem(DendromicaMaterials.EURENITE, 1.5F, -3.0F, new Item.Settings().group(ItemGroup.TOOLS));
-        ToolItem EURENITE_PICKAXE = new PickaxeItem(DendromicaMaterials.EURENITE, 2, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
-        ToolItem EURENITE_HOE = new HoeItem(DendromicaMaterials.EURENITE, -20, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
-        ToolItem EURENITE_AXE = new AxeItem(DendromicaMaterials.EURENITE, 5, -3.3F, new Item.Settings().group(ItemGroup.TOOLS)) {};
-        Item EURENITE = new Item(new Item.Settings().group(ItemGroup.MISC));
-        Block EURENITE_ORE = new Block(FabricBlockSettings.of(Material.METAL)
+        EURENITE_SWORD = new SwordItem(DendromicaMaterials.EURENITE, 3, -2.6F, new Item.Settings().group(ItemGroup.COMBAT).group(DENDROMICA_GROUP));
+        EURENITE_SHOVEL = new ShovelItem(DendromicaMaterials.EURENITE, 1.5F, -3.0F, new Item.Settings().group(ItemGroup.TOOLS));
+        EURENITE_PICKAXE = new PickaxeItem(DendromicaMaterials.EURENITE, 2, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
+        EURENITE_HOE = new HoeItem(DendromicaMaterials.EURENITE, -20, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
+        EURENITE_AXE = new AxeItem(DendromicaMaterials.EURENITE, 5, -3.3F, new Item.Settings().group(ItemGroup.TOOLS)) {};
+        EURENITE = new Item(new Item.Settings().group(ItemGroup.MISC));
+        EURENITE_ORE = new Block(FabricBlockSettings.of(Material.METAL)
                 .hardness(5.0f)
                 .breakByTool(FabricToolTags.PICKAXES, 3)
                 .requiresTool());
-        final Block EURENITE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+        EURENITE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
                 .hardness(8.0f)
                 .breakByTool(FabricToolTags.PICKAXES, 3)
                 .requiresTool());
-        final ItemGroup DENDROMICA_GROUP = FabricItemGroupBuilder.create(
+        DENDROMICA_GROUP = FabricItemGroupBuilder.create(
                 new Identifier("tutorial", "other"))
                 .icon(() -> new ItemStack(EURENITE))
                 .build();
